51447c189f16907dc8bb9c60b6d149a9553c59d8
Jakob Asslaender <jakob.asslaender@nyumc.org>
2023-05-05 11:27:38 -0400

    Add density compesation options

--- src/CoilMaps.jl
+++ src/CoilMaps.jl
@@ -1,4 +1,4 @@
-function calcCoilMaps(data::AbstractArray{Complex{T},3}, trj::AbstractVector{<:AbstractMatrix{T}}, U::AbstractMatrix{Complex{T}}, img_shape::NTuple{N,Int}; kernel_size = ntuple(_->6, N), calib_size =  ntuple(_->24, N), eigThresh_1=0.04, eigThresh_2=0.0, nmaps=1, verbose = false) where {N,T}
+function calcCoilMaps(data::AbstractArray{Complex{T},3}, trj::AbstractVector{<:AbstractMatrix{T}}, U::AbstractMatrix{Complex{T}}, img_shape::NTuple{N,Int}; density_compensation::Union{Symbol, <:AbstractVector{<:AbstractVector{T}}}=:radial_3D, kernel_size = ntuple(_->6, N), calib_size =  ntuple(_->24, N), eigThresh_1=0.04, eigThresh_2=0.0, nmaps=1, verbose = false) where {N,T}
     Ncoils = size(data,3)
     Ndims = length(img_shape)
     imdims = ntuple(i->i, Ndims)
@@ -9,9 +9,27 @@ function calcCoilMaps(data::AbstractArray{Complex{T},3}, trj::AbstractVector{<:A
     dataU = similar(@view data[:,:,1]) # size = Ncycles*Nr x Nt
     img_idx = CartesianIndices(img_shape)
     t = @elapsed for icoil ∈ axes(data,3)
-        @simd for i ∈ CartesianIndices(dataU)
-            dataU[i] = data[i,icoil] * conj(U[i[2],1]) * sum(abs2, @view trj[i[2]][:,i[1]])
+        if density_compensation == :radial_3D
+            @simd for i ∈ CartesianIndices(dataU)
+                dataU[i] = data[i,icoil] * conj(U[i[2],1]) * sum(abs2, @view trj[i[2]][:,i[1]])
+            end
+        elseif density_compensation == :radial_2D
+            @simd for i ∈ CartesianIndices(dataU)
+                dataU[i] = data[i,icoil] * conj(U[i[2],1]) * sum(abs, @view trj[i[2]][:,i[1]])
+            end
+        elseif density_compensation == :none
+            # no density compensation; premultiply data with inverse of sampling density before calling function
+            @simd for i ∈ CartesianIndices(dataU)
+                dataU[i] = data[i,icoil] * conj(U[i[2],1])
+            end
+        elseif isa(density_compensation, Symbol)
+            error("`density_compensation` can only be `:radial_3D`, `:radial_2D`, `:none`, or of type  `AbstractVector{<:AbstractVector{T}}`")
+        else
+            @simd for i ∈ CartesianIndices(dataU)
+                dataU[i] = data[i,icoil] * conj(U[i[2],1]) * density_compensation[i[2]][i[1]]
+            end
         end
+
         @views mul!(xbp[img_idx,icoil], adjoint(p), vec(dataU))
     end
     verbose && println("BP for coils maps: $t s")
