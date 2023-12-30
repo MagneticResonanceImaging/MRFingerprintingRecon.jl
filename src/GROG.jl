eab545c84642d3ac818ca556d41855eaf3464351
Jakob Asslaender <jakob.asslaender@nyumc.org>
2023-12-30 17:33:40 -0500

    Fix nyquist vs. sampling rate bug; fix tests

--- src/GROG.jl
+++ src/GROG.jl
@@ -3,111 +3,107 @@ function scGROG(data::AbstractArray{Complex{T}}, trj) where {T}
     # doi: 10.1002/mrm.21565
 
     # data should be passed with dimensions Nr x Ns x Ncoil
-    Nr = size(data,1) #number of readout points
+    Nr = size(data, 1) #number of readout points
     Ns = size(data, 2) # number of spokes across whole trajectory
     Ncoil = size(data, 3)
-    Nd = size(trj[1],1) # number of dimensions
+    Nd = size(trj[1], 1) # number of dimensions
     @assert Nr > Ncoil "Ncoil < Nr, problem is ill posed"
     @assert Ns > Ncoil^2 "Number of spokes < Ncoil^2, problem is ill posed"
 
     # preallocations
-    G = Array{Complex{T}}(undef, Nd, Ncoil, Ncoil) #matrix of GROG operators
+    lnG = Array{Complex{T}}(undef, Nd, Ncoil, Ncoil) #matrix of GROG operators
     vθ = Array{Complex{T}}(undef, Ns, Ncoil, Ncoil)
-    Gθ = [Array{Complex{T}}(undef, Ncoil, Ncoil) for _ = 1:Threads.nthreads()]
 
     # 1) Precompute n, m for the trajectory
     trjr = reshape(combinedimsview(trj), Nd, Nr, :)
-    nm = dropdims(diff(trjr[:,1:2,:],dims=2),dims=2)' .* Nr #nyquist units
+    nm = dropdims(diff(trjr[:, 1:2, :], dims=2), dims=2)' .* Nr # units of sampling rate
 
     # 2) For each spoke, solve Eq3 for Gθ and compute matrix log
-    Threads.@threads for ip ∈ axes(data,2)
-        @views Gθ[Threads.threadid()] .= transpose(data[1:end-1,ip,:] \ data[2:end,ip,:])
-        @views vθ[ip,:,:] .= log(Gθ[Threads.threadid()]) # matrix log
+    Threads.@threads for ip ∈ axes(data, 2)
+        @views Gθ = transpose(data[1:end-1, ip, :] \ data[2:end, ip, :])
+        vθ[ip, :, :] = log(Gθ) # matrix log
     end
 
     # 3) Solve Eq8 Nc^2 times
-    Threads.@threads for i ∈ CartesianIndices(@view G[1,:,:])
-        @views G[:,i] .= nm \ vθ[:,i]
+    Threads.@threads for i ∈ CartesianIndices(@view lnG[1, :, :])
+        @views lnG[:, i] .= nm \ vθ[:, i]
     end
 
-    # 4) Use Eq9 to form Gx, Gy, Gz
-    Threads.@threads for id ∈ axes(G,1)
-        @views G[id,:,:] .= exp(G[id,:,:]) #matrix exponent
-    end
+    lnG = [lnG[id, :, :] for id = 1:Nd]
 
-    G = [G[id,:,:] for id=1:Nd]
-
-    return G
+    return lnG
 end
 
-function griddedBackProjection(data::AbstractArray{Complex{T}}, G, trj, U::Matrix{Complex{T}}, cmaps=(1,); shape = ntuple(_ -> size(data, 1)÷2, size(trj[1],1)), density = false, verbose = false) where {T}
+function griddedBackProjection(data::AbstractArray{Complex{T}}, lnG, trj, U::Matrix{Complex{T}}, cmaps; density=false, verbose=false) where {T}
     # performs GROG gridding, returns backprojection and kernels
     # assumes data is passed with dimensions Nr x NCyc*Nt x Ncoil
-    lG = [log(Gi) for Gi ∈ G]
 
+    img_shape = size(cmaps[1])
+    Nr = size(data, 1) #number of readout points
     Nt = length(trj) # number of time points
     @assert Nt == size(U, 1) "Mismatch between trajectory and basis"
     Ncoeff = size(U, 2)
 
-    idx = CartesianIndices(shape)
+    idx = CartesianIndices(img_shape)
     Ncoil = length(cmaps)
     data = reshape(data, :, Nt, Ncoil) # make sure data has correct size before gridding
 
     # preallocations
-    dataU = zeros(Complex{T}, shape..., Ncoil, Ncoeff)
-    Λ = zeros(Complex{T}, Ncoeff, Ncoeff, shape...)
+    dataU = zeros(Complex{T}, img_shape..., Ncoil, Ncoeff)
+    Λ = zeros(Complex{T}, Ncoeff, Ncoeff, img_shape...)
     if density
-        D = zeros(Int16, shape..., Nt)
+        D = zeros(Int16, img_shape..., Nt)
     end
 
-    method = ExpMethodHigham2005()
-    cache   = [ExponentialUtilities.alloc_mem(lG[1], method) for _ ∈ 1:Threads.nthreads()]
-    lGcache = [similar(lG[1]) for _ ∈ 1:Threads.nthreads()]
+    exp_method = ExpMethodHigham2005()
+    cache = [ExponentialUtilities.alloc_mem(lnG[1], exp_method) for _ ∈ 1:Threads.nthreads()]
+    lGcache = [similar(lnG[1]) for _ ∈ 1:Threads.nthreads()]
 
     # gridding backprojection & kernel calculation
     t = @elapsed begin
-        Threads.@threads for i ∈ CartesianIndices(@view data[:,:,1])
+        Threads.@threads for i ∈ CartesianIndices(@view data[:, :, 1])
             idt = Threads.threadid()
-            for j ∈ eachindex(shape)
-                trj_i = trj[i[2]][j,i[1]] * shape[j] + 1/2
-                ig = round(trj_i)
-                shift = ig - trj_i
-                trj[i[2]][j,i[1]] = ig + shape[j] ÷ 2
+            for j ∈ length(img_shape):-1:1
+                trj_i = trj[i[2]][j, i[1]] * img_shape[j] + 1 / 2
+                k_idx = round(trj_i)
+                shift = (k_idx - trj_i) * Nr/img_shape[j]
+                trj[i[2]][j, i[1]] = k_idx + img_shape[j] ÷ 2
 
-                lGcache[idt] .= shift .* lG[j]
-                @views data[i,:] = exponential!(lGcache[idt], method, cache[idt]) * data[i,:]
+                lGcache[idt] .= shift .* lnG[j]
+                @views data[i, :] = exponential!(lGcache[idt], exp_method, cache[idt]) * data[i, :]
             end
         end
 
-        for i ∈ CartesianIndices(@view data[:,:,1])
-            ig = CartesianIndex(ntuple(j -> Int(trj[i[2]][j,i[1]]), length(shape)))
+        for i ∈ CartesianIndices(@view data[:, :, 1])
+            k_idx = CartesianIndex(ntuple(j -> Int(trj[i[2]][j, i[1]]), length(img_shape)))
 
             # multiply by basis for backprojection
-            for icoef ∈ axes(U,2)
-                @views dataU[ig,:,icoef] .+= data[i[1],i[2],:] .* conj(U[i[2],icoef])
+            for icoef ∈ axes(U, 2)
+                @views dataU[k_idx, :, icoef] .+= data[i[1], i[2], :] .* conj(U[i[2], icoef])
             end
             # add to kernel
             for ic ∈ CartesianIndices((Ncoeff, Ncoeff))
-                Λ[ic[1],ic[2],ig] += conj.(U[i[2],ic[1]]) * U[i[2],ic[2]]
+                Λ[ic[1], ic[2], k_idx] += conj.(U[i[2], ic[1]]) * U[i[2], ic[2]]
             end
             if density
-                D[ig,i[2]] += 1
+                D[k_idx, i[2]] += 1
             end
         end
     end
-    verbose && println("Gridding time: t = $t s"); flush(stdout)
-    Λ .= ifftshift(Λ, 3:(3+length(shape)-1))
+    verbose && println("Gridding time: t = $t s")
+    flush(stdout)
+    Λ .= ifftshift(Λ, 3:(3+length(img_shape)-1))
 
     # compute backprojection
-    xbp = zeros(Complex{T}, shape..., Ncoeff)
-    xbpci = [Array{Complex{T}}(undef, shape...) for _ = 1:Threads.nthreads()]
-    Threads.@threads for icoef ∈ axes(U,2)
+    xbp = zeros(Complex{T}, img_shape..., Ncoeff)
+    xbpci = [Array{Complex{T}}(undef, img_shape...) for _ = 1:Threads.nthreads()]
+    Threads.@threads for icoef ∈ axes(U, 2)
         idt = Threads.threadid()
         for icoil ∈ eachindex(cmaps)
-            @views ifftshift!(xbpci[idt], dataU[idx,icoil,icoef])
+            @views ifftshift!(xbpci[idt], dataU[idx, icoil, icoef])
             ifft!(xbpci[idt])
             xbpci[idt] = fftshift(xbpci[idt])
-            xbp[idx,icoef] .+= conj.(cmaps[icoil]) .* xbpci[idt]
+            xbp[idx, icoef] .+= conj.(cmaps[icoil]) .* xbpci[idt]
         end
     end
 
--- test/reconstruct_grog.jl
+++ test/reconstruct_grog.jl
@@ -11,18 +11,20 @@ using Test
 ## set parameters
 T  = Float32
 Nx = 64
-Nt = 5
-Nc = 2
-Ncyc = 100
+Nr = 2Nx
+Nc = 4
+Nt = 100
+Ncyc = 500
 Ncoil = 9
 
 ## create test image
 x = zeros(Complex{T}, Nx, Nx, Nc)
 x[:,:,1] = transpose(shepp_logan(Nx))
 x[1:end÷2,:,1] .*= exp(1im * π/3)
+x[:,:,2] = shepp_logan(Nx)
 
 ## coil maps
-cmaps = zeros(Complex{T}, Nx, Nx, Ncoil)
+cmaps = ones(Complex{T}, Nx, Nx, Ncoil)
 cmaps[:,:,1] .= phantom(1:Nx, 1:Nx, [gauss2((Nx÷8,  Nx÷8),  (Nx÷1.5,Nx÷1.5))], 2)
 cmaps[:,:,2] .= phantom(1:Nx, 1:Nx, [gauss2((Nx÷8,  Nx÷2),  (Nx÷1.5,Nx÷1.5))], 2)
 cmaps[:,:,3] .= phantom(1:Nx, 1:Nx, [gauss2((Nx÷8,  7Nx÷8), (Nx÷1.5,Nx÷1.5))], 2)
@@ -32,24 +34,34 @@ cmaps[:,:,6] .= phantom(1:Nx, 1:Nx, [gauss2((Nx÷2,  7Nx÷8), (Nx÷1.5,Nx÷1.5))
 cmaps[:,:,7] .= phantom(1:Nx, 1:Nx, [gauss2((7Nx÷8, Nx÷8),  (Nx÷1.5,Nx÷1.5))], 2)
 cmaps[:,:,8] .= phantom(1:Nx, 1:Nx, [gauss2((7Nx÷8, Nx÷2),  (Nx÷1.5,Nx÷1.5))], 2)
 cmaps[:,:,9] .= phantom(1:Nx, 1:Nx, [gauss2((7Nx÷8, 7Nx÷8), (Nx÷1.5,Nx÷1.5))], 2)
+
+# [cmaps[i,:,2] .*= exp( 1im * π/4 * i/Nx) for i ∈ axes(cmaps,1)]
+# [cmaps[i,:,3] .*= exp(-1im * π/4 * i/Nx) for i ∈ axes(cmaps,1)]
+# [cmaps[:,i,4] .*= exp( 1im * π/4 * i/Nx) for i ∈ axes(cmaps,2)]
+# [cmaps[:,i,5] .*= exp(-1im * π/4 * i/Nx) for i ∈ axes(cmaps,2)]
+# [cmaps[i,:,6] .*= exp( 2im * π/4 * i/Nx) for i ∈ axes(cmaps,1)]
+# [cmaps[i,:,7] .*= exp(-2im * π/4 * i/Nx) for i ∈ axes(cmaps,1)]
+# [cmaps[:,i,8] .*= exp( 2im * π/4 * i/Nx) for i ∈ axes(cmaps,2)]
+# [cmaps[:,i,9] .*= exp(-2im * π/4 * i/Nx) for i ∈ axes(cmaps,2)]
+
 for i ∈ CartesianIndices(@view cmaps[:,:,1])
     cmaps[i,:] ./= norm(cmaps[i,:])
 end
 cmaps = [cmaps[:,:,ic] for ic=1:Ncoil]
 
-## set up basis functions
-U = randn(Complex{T}, Nt, Nc)
-U,_,_ = svd(U)
-
 ## set up trajectory
 α_g = 2π / (1+√5)
-phi = α_g * (1:Nt*Ncyc)
+phi = α_g * (0:Nt*Ncyc-1)
 theta = 0 * (1:Nt*Ncyc) .+ pi/2
 phi = reshape(phi, Ncyc, Nt)
 theta = reshape(theta, Ncyc, Nt)
 
-trj = kooshball(2Nx, theta, phi; T = T)
+trj = kooshball(Nr, theta, phi; T = T)
 trj = [trj[i][1:2,:] for i ∈ eachindex(trj)]
+
+## set up basis functions
+U = randn(Complex{T}, Nt, Nc)
+U,_,_ = svd(U)
 
 ## simulate data
 data = Array{Complex{T}}(undef, size(trj[1], 2), Nt, Ncoil)
@@ -57,7 +69,7 @@ nfftplan = plan_nfft(trj[1], (Nx,Nx))
 xcoil = copy(x)
 for icoil ∈ 1:Ncoil
     xcoil .= x
-    xcoil[:,:,1] .*= cmaps[icoil]
+    xcoil .*= cmaps[icoil]
     for it ∈ axes(data,2)
         nodes!(nfftplan, trj[it])
         xt = reshape(reshape(xcoil, :, Nc) * U[it,:], Nx, Nx)
@@ -66,21 +78,27 @@ for icoil ∈ 1:Ncoil
 end
 
 ## self-calibrating radial GROG
-G = scGROG(reshape(data, 2Nx, :, Ncoil), trj)
+lnG = scGROG(reshape(data, Nr, :, Ncoil), trj)
 
 ## GROG Reconstruction
-xbp_grog, Λ, D = griddedBackProjection(reshape(copy(data), 2Nx, :, Ncoil), G, deepcopy(trj), U, cmaps; density=true)
-A_grog_default = FFTNormalOpBasisFuncLO((Nx,Nx), U; cmaps=cmaps, D=D)
+xbp_grog, Λ, D = griddedBackProjection(reshape(copy(data), Nr, :, Ncoil), lnG, deepcopy(trj), U, cmaps; density=true)
+
 A_grog_efficient = FFTNormalOpBasisFuncLO((Nx,Nx), U; cmaps=cmaps, Λ=Λ)
-xg = cg(A_grog_efficient, vec(xbp_grog), maxiter=100)
+xg = cg(A_grog_efficient, vec(xbp_grog), maxiter=20)
 xg = reshape(xg, Nx, Nx, Nc)
-xgd = cg(A_grog_default, vec(xbp_grog), maxiter=100)
+
+A_grog_default = FFTNormalOpBasisFuncLO((Nx,Nx), U; cmaps=cmaps, D=D)
+xgd = cg(A_grog_default, vec(xbp_grog), maxiter=20)
 xgd = reshape(xgd, Nx, Nx, Nc)
+
+## Fix irrelevant phase slope
+[xg[i,j,:]  .*= -exp(1im * π * (i + j)/Nx) for i = 1:Nx, j = 1:Nx]
+[xgd[i,j,:] .*= -exp(1im * π * (i + j)/Nx) for i = 1:Nx, j = 1:Nx]
 
 ## NFFT Reconstruction
 xbp_rad = calculateBackProjection(data, trj, U, cmaps)
 A_rad = NFFTNormalOpBasisFuncLO((Nx,Nx), trj, U; cmaps=cmaps)
-xr = cg(A_rad, vec(xbp_rad), maxiter=100)
+xr = cg(A_rad, vec(xbp_rad), maxiter=20)
 xr = reshape(xr, Nx, Nx, Nc)
 
 ## crop x
@@ -93,31 +111,24 @@ end
 xc = ifft(ifftshift(xc, 1:2), 1:2)
 
 ## test recon equivalence
-mask = abs.(x[:,:,1]) .> 0
-@test abs.(xc[mask,:]) ≈ abs.(xr[mask,:])  rtol = 1e-1
-@test abs.(xc[mask,:]) ≈ abs.(xg[mask,:])  rtol = 1e-1
-@test abs.(xc[mask,:]) ≈ abs.(xgd[mask,:]) rtol = 1e-1
-@test abs.(xg[mask,:]) ≈ abs.(xgd[mask,:]) rtol = 1e-2
+@test xc ≈ xr  rtol = 1e-1
+@test xc ≈ xg  rtol = 2e-1
+@test xc ≈ xgd rtol = 2e-1
+@test xg ≈ xgd rtol = 1e-2
 
 ## test equivalence of efficient kernel calculation
-for i ∈ CartesianIndices((Nx, Nx))
-    @test A_grog_default.prod!.A.Λ[:,:,i] ≈ Λ[:,:,i] rtol = 1e-1
-end
+@test A_grog_default.prod!.A.Λ ≈ Λ
 
 ## test GROG kernels for 1st spoke in trajectory
-data = reshape(data, 2Nx, :, Ncoil)
-trjr = reshape(combinedimsview(trj), 2, 2Nx, :)
-nm = dropdims(diff(trjr[:,1:2,:],dims=2),dims=2)' .* 2Nx #nyquist units
-data_temp = Array{Complex{T}}(undef, Ncoil)
-for j = 1:2Nx-1
-    data_temp .= data[j,1,:]
-    for k = 2:-1:1
-        data_temp .= G[k]^nm[k] * data_temp
-    end
-    @test data_temp ≈ data[j+1,1,:] rtol = 5e-1
+data = reshape(data, Nr, :, Ncoil)
+trjr = reshape(combinedimsview(trj), 2, Nr, :)
+
+Threads.@threads for ispoke in axes(data,2)
+    nm = dropdims(diff(trjr[:,1:2,ispoke],dims=2),dims=2) .* Nr
+    @test [data[j,ispoke,:][ic] for j in 2:Nr, ic = 1:Ncoil] ≈ [(exp(nm[1] * lnG[1]) * exp(nm[2] * lnG[2]) * data[j,ispoke,:])[ic] for j in 1:Nr-1, ic =1:Ncoil] rtol = 3e-1
 end
 
 ##
 # using Plots
-# plot(heatmap(abs.(cat(xc[:,:,1], xr[:,:,1], xg[:,:,1], xgd[:,:,1], dims=2)), aspect_ratio=1), size=(1000,400), clim=(0,1.5))
-# plot(heatmap(abs.(xg[:,:,1] ./ xr[:,:,1]), aspect_ratio=1), size=(1000,400), clim=(0.75,1.25))
+# heatmap(abs.(cat(reshape(xc, Nx, :), reshape(xr, Nx, :), reshape(xg, Nx, :), reshape(xgd, Nx, :), dims=1)), clim=(0.75, 1.25), size=(1100,800))
+# heatmap(angle.(cat(reshape(xc, Nx, :), reshape(xr, Nx, :), reshape(xg, Nx, :), reshape(xgd, Nx, :), dims=1)), clim=(0.75, 1.25), size=(1100,800))
