79610bbc70a5b922411897b0c312b137df4e5ca5
Sebastian Flassbeck <sebastian.flassbeck@nyulangone.org>
2024-02-16 16:58:02 -0500

    Made call of calcCoilMaps consistent with call of calculateBackProjection

--- src/CoilMaps.jl
+++ src/CoilMaps.jl
@@ -1,4 +1,4 @@
-function calcCoilMaps(data::AbstractArray{Complex{T},3}, trj::AbstractVector{<:AbstractMatrix{T}}, U::AbstractMatrix{Complex{T}}, img_shape::NTuple{N,Int}; density_compensation=:radial_3D, kernel_size=ntuple(_ -> 6, N), calib_size=ntuple(_ -> 24, N), eigThresh_1=0.01, eigThresh_2=0.9, nmaps=1, verbose=false) where {N,T}
+function calcCoilMaps(data::AbstractArray{Complex{T},3}, trj::AbstractVector{<:AbstractMatrix{T}}, img_shape::NTuple{N,Int}; U = N==3 ? I(size(data,2)) : I(1), density_compensation=:radial_3D, kernel_size=ntuple(_ -> 6, N), calib_size=ntuple(_ -> 24, N), eigThresh_1=0.01, eigThresh_2=0.9, nmaps=1, verbose=false) where {N,T}
     Ncoils = size(data, 3)
     Ndims = length(img_shape)
     imdims = ntuple(i -> i, Ndims)
